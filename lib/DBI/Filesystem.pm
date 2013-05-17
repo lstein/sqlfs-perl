fbc3f00541b03bdca74323da0bf6b9e94bb3e2fc
Lincoln Stein <lincoln.stein@gmail.com>
2013-05-17 09:27:50 -0500

    add framework files

---
+++ lib/DBI/Filesystem.pm
@@ -0,0 +1,27 @@
+package DBI::Filesystem;
+
+=head1 NAME
+
+DBI::Filesystem - Store a filesystem in a relational database
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 AUTHOR
+
+Copyright 2013, Lincoln D. Stein <lincoln.stein@gmail.com>
+
+=head1 LICENSE
+
+This package is distributed under the terms of the Perl Artistic
+License 2.0. See http://www.perlfoundation.org/artistic_license_2_0.
+
+=cut
+
+
+1;
+
+__END__
